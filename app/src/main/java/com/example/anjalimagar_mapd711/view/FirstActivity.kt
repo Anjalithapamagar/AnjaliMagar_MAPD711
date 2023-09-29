bacac4463f2e85e32155f13c337a306a6fc6a6a6
Anjali <anjalimagar666@gmail.com>
2023-09-29 09:19:35 -0400

    Initial commit

---
+++ app/src/main/java/com/example/anjalimagar_mapd711/view/FirstActivity.kt
@@ -0,0 +1,24 @@
+package com.example.anjalimagar_mapd711.view
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.anjalimagar_mapd711.R
+
+
+class FirstActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
